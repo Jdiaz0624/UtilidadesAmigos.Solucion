662e7ba81b3e97f8c966c8572bba4036b47e7f07
juan.diaz <juan.diaz@TI-001>
2019-11-12 19:52:20 -0400

    f

--- UtilidadesAmigos.Solucion/Paginas/ProduccionDiaria.aspx.cs
+++ UtilidadesAmigos.Solucion/Paginas/ProduccionDiaria.aspx.cs
@@ -6,10 +6,6 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 //EXPORTAMOS A EXEL
 using System.IO;
-using iTextSharp.text;
-using iTextSharp.text.html;
-using iTextSharp.text.pdf;
-using iTextSharp.text.html.simpleparser;
 using System.Drawing;
 
 
