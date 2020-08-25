a4ba0ae1368a735b4db9146ae797c44a8727d055
dd <d>
2020-08-25 16:10:08 -0400

    dd

---
+++ UtilidadesAmigos.Logica/Entidades/EGRACY.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilidadesAmigos.Logica.Entidades
+{
+    public class EGRACY
+    {
+        public System.Nullable<decimal> Numero { get; set; }
+
+        public string Cedula { get; set; }
+
+        public string NotasCredito { get; set; }
+
+        public string FechaComprobante { get; set; }
+
+        public string NCfAfectado { get; set; }
+
+        public string Fecha_NCf_Afectado { get; set; }
+
+        public decimal? Valor { get; set; }
+    }
+}
---
+++ UtilidadesAmigos.Logica/Entidades/EGracy0.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilidadesAmigos.Logica.Entidades
+{
+    public class EGracy0
+    {
+        public string NCF { get; set; }
+
+        public decimal Valor { get; set; }
+    }
+}
