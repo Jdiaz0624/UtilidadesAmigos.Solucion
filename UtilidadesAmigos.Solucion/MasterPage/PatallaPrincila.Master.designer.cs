42de862659ffcaea449c69e5b1970e3afe426d0d
dd <d>
2021-03-02 16:55:35 -0400

    ff

--- UtilidadesAmigos.Solucion/MasterPage/PatallaPrincila.Master.designer.cs
+++ UtilidadesAmigos.Solucion/MasterPage/PatallaPrincila.Master.designer.cs
@@ -42,15 +42,6 @@ namespace UtilidadesAmigos.Solucion.MasterPage
         protected global::System.Web.UI.WebControls.LinkButton LinkInicio;
 
         /// <summary>
-        /// linkTicket control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton linkTicket;
-
-        /// <summary>
         /// LinkConsultarPersonas control.
         /// </summary>
         /// <remarks>
