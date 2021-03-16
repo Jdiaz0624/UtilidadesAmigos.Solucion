3a4275a5ba240b84f5cf319349ebc5fc97bb0475
dd <d>
2021-03-16 11:24:27 -0400

    Cambio

--- UtilidadesAmigos.Solucion/MasterPage/PatallaPrincila.Master.cs
+++ UtilidadesAmigos.Solucion/MasterPage/PatallaPrincila.Master.cs
@@ -317,7 +317,7 @@ namespace UtilidadesAmigos.Solucion.MasterPage
         {
             if (Session["IdUsuario"] != null)
             {
-                Response.Redirect("~/Paginas/GenerarBakupBD.aspx");
+                Response.Redirect("~/Paginas/Procesos/GenerarBakupBD.aspx");
             }
             else
             {
