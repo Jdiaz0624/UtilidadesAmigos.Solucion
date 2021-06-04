8ba968d73579cec990eb0e483b40b9e2f2052a4b
Ing. Juan Marcelino <Ing. Juan Marcelino@JDIAZ0624>
2021-06-04 16:38:18 -0400

    s

--- UtilidadesAmigos.Logica/Comunes/ExportarReportePDF.cs
+++ UtilidadesAmigos.Logica/Comunes/ExportarReportePDF.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-using CrystalDecisions.CrystalReports.Engine;
-using CrystalDecisions.ReportSource;
-using CrystalDecisions.Shared;
+//using CrystalDecisions.CrystalReports.Engine;
+//using CrystalDecisions.ReportSource;
+//using CrystalDecisions.Shared;
 using System.Data;
 using System.IO;
 using System.Web;
@@ -42,23 +42,23 @@ namespace UtilidadesAmigos.Logica.Comunes
 
        
 
-        public void ExportarReporte() {
-            ReportDocument Factura = new ReportDocument();
+        //public void ExportarReporte() {
+        //    ReportDocument Factura = new ReportDocument();
 
-            SqlCommand comando = new SqlCommand();
-            comando.CommandText = "EXEC [Utililades].[SP_GENERAR_REPORTE_DATOS_FACTURAS_PDF] @IdComprobante";
-            comando.Connection = UtilidadesAmigos.Data.Conexiones.ADO.BDConexion.ObtenerConexion();
+        //    SqlCommand comando = new SqlCommand();
+        //    comando.CommandText = "EXEC [Utililades].[SP_GENERAR_REPORTE_DATOS_FACTURAS_PDF] @IdComprobante";
+        //    comando.Connection = UtilidadesAmigos.Data.Conexiones.ADO.BDConexion.ObtenerConexion();
 
-            comando.Parameters.Add("@IdComprobante", SqlDbType.Decimal);
-            comando.Parameters["@IdComprobante"].Value = IdComprobante;
+        //    comando.Parameters.Add("@IdComprobante", SqlDbType.Decimal);
+        //    comando.Parameters["@IdComprobante"].Value = IdComprobante;
 
-            Factura.Load(RutaReporte);
-            Factura.Refresh();
-            Factura.SetParameterValue("@IdComprobante", IdComprobante);
-            Factura.SetDatabaseLogon(UsuaruoBD, ClaveBD);
-            Factura.ExportToHttpResponse(ExportFormatType.PortableDocFormat, Response, true, NombreArchivo);
-            //  Factura.PrintToPrinter(1, false, 0, 1);
-            //  crystalReportViewer1.ReportSource = Factura;
+        //    Factura.Load(RutaReporte);
+        //    Factura.Refresh();
+        //    Factura.SetParameterValue("@IdComprobante", IdComprobante);
+        //    Factura.SetDatabaseLogon(UsuaruoBD, ClaveBD);
+        //    Factura.ExportToHttpResponse(ExportFormatType.PortableDocFormat, Response, true, NombreArchivo);
+        //    //  Factura.PrintToPrinter(1, false, 0, 1);
+        //    //  crystalReportViewer1.ReportSource = Factura;
         }
     }
-}
+//}
