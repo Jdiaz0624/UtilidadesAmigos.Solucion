d1074ff4765b701d39b5d744af2f17b68795629b
JuanMedina <JuanMedina@JDIAZ0624>
2022-03-31 22:50:28 -0400

    d

--- UtilidadesAmigos.Solucion/Paginas/Reportes/ComisionesIntermediarios.aspx.cs
+++ UtilidadesAmigos.Solucion/Paginas/Reportes/ComisionesIntermediarios.aspx.cs
@@ -158,6 +158,138 @@ namespace UtilidadesAmigos.Solucion.Paginas
 
         }
         #endregion
+        #region CONTROL DE PAGINACION DE LAS COMISIONES ACUMULADAS
+        readonly PagedDataSource pagedDataSource_Comisiones_Acumuladas = new PagedDataSource();
+        int _PrimeraPagina_Comisiones_Acumuladas, _UltimaPagina_Comisiones_Acumuladas;
+        private int _TamanioPagina_Comisiones_Acumuladas = 10;
+        private int CurrentPage_Comisiones_Acumuladas
+        {
+            get
+            {
+                if (ViewState["CurrentPage"] == null)
+                {
+                    return 0;
+                }
+                return ((int)ViewState["CurrentPage"]);
+            }
+            set
+            {
+                ViewState["CurrentPage"] = value;
+            }
+
+        }
+
+        private void HandlePaging_Comisiones_Acumuladas(ref DataList NombreDataList, ref Label LbPaginaActual)
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("IndicePagina"); //Start from 0
+            dt.Columns.Add("TextoPagina"); //Start from 1
+
+            _PrimeraPagina_Comisiones_Acumuladas = CurrentPage_Comisiones_Acumuladas - 5;
+            if (CurrentPage_Comisiones_Acumuladas > 5)
+                _UltimaPagina_Comisiones_Acumuladas = CurrentPage_Comisiones_Acumuladas + 5;
+            else
+                _UltimaPagina_Comisiones_Acumuladas = 10;
+
+            // Check last page is greater than total page then reduced it to total no. of page is last index
+            if (_UltimaPagina_Comisiones_Acumuladas > Convert.ToInt32(ViewState["TotalPages"]))
+            {
+                _UltimaPagina_Comisiones_Acumuladas = Convert.ToInt32(ViewState["TotalPages"]);
+                _PrimeraPagina_Comisiones_Acumuladas = _UltimaPagina_Comisiones_Acumuladas - 10;
+            }
+
+            if (_PrimeraPagina_Comisiones_Acumuladas < 0)
+                _PrimeraPagina_Comisiones_Acumuladas = 0;
+
+            //AGREGAMOS LA PAGINA EN LA QUE ESTAMOS
+            int NumeroPagina = (int)CurrentPage_Comisiones_Acumuladas;
+            LbPaginaActual.Text = (NumeroPagina + 1).ToString();
+            // Now creating page number based on above first and last page index
+            for (var i = _PrimeraPagina_Comisiones_Acumuladas; i < _UltimaPagina_Comisiones_Acumuladas; i++)
+            {
+                var dr = dt.NewRow();
+                dr[0] = i;
+                dr[1] = i + 1;
+                dt.Rows.Add(dr);
+            }
+
+
+            NombreDataList.DataSource = dt;
+            NombreDataList.DataBind();
+        }
+
+        private void Paginar_Comisiones_Acumuladas(ref Repeater RptGrid, IEnumerable<object> Listado, int _NumeroRegistros, ref Label lbCantidadPagina, ref ImageButton PrimeraPagina, ref ImageButton PaginaAnterior, ref ImageButton SiguientePagina, ref ImageButton UltimaPagina)
+        {
+            pagedDataSource_Comisiones_Acumuladas.DataSource = Listado;
+            pagedDataSource_Comisiones_Acumuladas.AllowPaging = true;
+
+            ViewState["TotalPages"] = pagedDataSource_Comisiones_Acumuladas.PageCount;
+            // lbNumeroVariable.Text = "1";
+            lbCantidadPagina.Text = pagedDataSource_Comisiones_Acumuladas.PageCount.ToString();
+
+            //MOSTRAMOS LA CANTIDAD DE PAGINAS A MOSTRAR O NUMERO DE REGISTROS
+            pagedDataSource_Comisiones_Acumuladas.PageSize = (_NumeroRegistros == 0 ? _TamanioPagina_Comisiones_Acumuladas : _NumeroRegistros);
+            pagedDataSource_Comisiones_Acumuladas.CurrentPageIndex = CurrentPage_Comisiones_Acumuladas;
+
+            //HABILITAMOS LOS BOTONES DE LA PAGINACION
+            PrimeraPagina.Enabled = !pagedDataSource_Comisiones_Acumuladas.IsFirstPage;
+            PaginaAnterior.Enabled = !pagedDataSource_Comisiones_Acumuladas.IsFirstPage;
+            SiguientePagina.Enabled = !pagedDataSource_Comisiones_Acumuladas.IsLastPage;
+            UltimaPagina.Enabled = !pagedDataSource_Comisiones_Acumuladas.IsLastPage;
+
+            RptGrid.DataSource = pagedDataSource_Comisiones_Acumuladas;
+            RptGrid.DataBind();
+
+
+            //divPaginacionComisionSupervisor.Visible = true;
+        }
+        enum OpcionesPaginacionValores_Comisiones_Acumuladas
+        {
+            PrimeraPagina = 1,
+            SiguientePagina = 2,
+            PaginaAnterior = 3,
+            UltimaPagina = 4
+        }
+        private void MoverValoresPaginacion_Comisiones_Acumuladas(int Accion, ref Label lbPaginaActual, ref Label lbCantidadPaginas)
+        {
+
+            int PaginaActual = 0;
+            switch (Accion)
+            {
+
+                case 1:
+                    //PRIMERA PAGINA
+                    lbPaginaActual.Text = "1";
+
+                    break;
+
+                case 2:
+                    //SEGUNDA PAGINA
+                    PaginaActual = Convert.ToInt32(lbPaginaActual.Text);
+                    PaginaActual++;
+                    lbPaginaActual.Text = PaginaActual.ToString();
+                    break;
+
+                case 3:
+                    //PAGINA ANTERIOR
+                    PaginaActual = Convert.ToInt32(lbPaginaActual.Text);
+                    if (PaginaActual > 1)
+                    {
+                        PaginaActual--;
+                        lbPaginaActual.Text = PaginaActual.ToString();
+                    }
+                    break;
+
+                case 4:
+                    //ULTIMA PAGINA
+                    lbPaginaActual.Text = lbCantidadPaginas.Text;
+                    break;
+
+
+            }
+
+        }
+        #endregion
 
         private void SacarElNombreDelIntermediario() {
             UtilidadesAmigos.Logica.Comunes.SacarNombreIntermediarioSupervisor SacarInformacion = new Logica.Comunes.SacarNombreIntermediarioSupervisor(txtCodigoIntermediarioComisiones.Text);
@@ -521,6 +653,23 @@ namespace UtilidadesAmigos.Solucion.Paginas
         }
         #endregion
 
+        #region MOSTRAR EL LISTADO DE LAS COMISIONES ACUMULADA
+        private void MostrarListadoComisionesAcumuladas() {
+            int? _Intermediario = string.IsNullOrEmpty(txtCodigoIntermediarioComisiones.Text.Trim()) ? new Nullable<int>() : Convert.ToInt32(txtCodigoIntermediarioComisiones.Text);
+            int? _Oficina = ddlSeleccionaroficinaComisiones.SelectedValue != "-1" ? Convert.ToInt32(ddlSeleccionaroficinaComisiones.SelectedValue) : new Nullable<int>();
+            int? _Ramo = ddlSeleccionarRamo.SelectedValue != "-1" ? Convert.ToInt32(ddlSeleccionarRamo.SelectedValue) : new Nullable<int>();
+
+            var ComisionesAcumuladas = ObjDataConexion.Value.ComisionesAcumuladasIntermediarios(
+                _Intermediario,
+                _Oficina,
+                _Ramo,
+                null);
+            Paginar_Comisiones_Acumuladas(ref rpListadoMontosIntermediariosAcumulados, ComisionesAcumuladas, 10, ref lbCantidadPaginaVariableDetalle, ref btnPrimeraPAginaMontosAcumulados, ref btnPaginaAnteriorMontosAcumulados, ref btnSiguientePaginaMontosAcumulados, ref btnUltimaPaginaMontosAcumulados);
+            HandlePaging_Comisiones_Acumuladas(ref DTPaginacionMontosAcumulados, ref LinkBlbPaginaActualVariavleDetalleutton2);
+         
+        }
+        #endregion
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack) {
@@ -554,6 +703,9 @@ namespace UtilidadesAmigos.Solucion.Paginas
 
         protected void DTPaginacionMontosAcumulados_ItemCommand(object source, DataListCommandEventArgs e)
         {
+            if (!e.CommandName.Equals("newPage")) return;
+            CurrentPage_Comisiones_Acumuladas = Convert.ToInt32(e.CommandArgument.ToString());
+            MostrarListadoComisionesAcumuladas();
 
         }
 
@@ -627,7 +779,24 @@ namespace UtilidadesAmigos.Solucion.Paginas
 
         protected void btnActualizarListadoNuevo_Click(object sender, ImageClickEventArgs e)
         {
-            
+            if (string.IsNullOrEmpty(txtFechaDesdeComisiones.Text.Trim()) || string.IsNullOrEmpty(txtFechaHastaComisiones.Text.Trim()))
+            {
+                ClientScript.RegisterStartupScript(GetType(), "CamposFechaVAcios()", "CamposFechaVAcios();", true);
+                if (string.IsNullOrEmpty(txtFechaDesdeComisiones.Text.Trim()))
+                {
+                    ClientScript.RegisterStartupScript(GetType(), "CampoFechaDesdeVacio()", "CampoFechaDesdeVacio();", true);
+                }
+                if (string.IsNullOrEmpty(txtFechaHastaComisiones.Text.Trim()))
+                {
+                    ClientScript.RegisterStartupScript(GetType(), "CampoFechaHastaVacio()", "CampoFechaHastaVacio();", true);
+                }
+            }
+            else
+            {
+                decimal IdUSuario = (decimal)Session["IdUsuario"];
+                ProcesarInformacionDataComisiones(IdUSuario);
+                MostrarListadoComisionesAcumuladas();
+            }
         }
 
         protected void btnPrimeraPaginaComisiones_Click(object sender, ImageClickEventArgs e)
@@ -667,22 +836,29 @@ namespace UtilidadesAmigos.Solucion.Paginas
 
         protected void btnPrimeraPAginaMontosAcumulados_Click(object sender, ImageClickEventArgs e)
         {
-
+            CurrentPage_Comisiones_Acumuladas = 0;
+            MostrarListadoComisionesAcumuladas();
         }
 
         protected void btnPaginaAnteriorMontosAcumulados_Click(object sender, ImageClickEventArgs e)
         {
-
+            CurrentPage_Comisiones_Acumuladas += -1;
+            MostrarListadoComisionesAcumuladas();
+            MoverValoresPaginacion_Comisiones_Acumuladas((int)OpcionesPaginacionValores_Comisiones_Acumuladas.PaginaAnterior, ref LinkBlbPaginaActualVariavleDetalleutton2, ref lbCantidadPaginaVariableDetalle);
         }
 
         protected void btnSiguientePaginaMontosAcumulados_Click(object sender, ImageClickEventArgs e)
         {
 
+            CurrentPage_Comisiones_Acumuladas += 1;
+            MostrarListadoComisionesAcumuladas();
         }
 
         protected void btnUltimaPaginaMontosAcumulados_Click(object sender, ImageClickEventArgs e)
         {
-
+            CurrentPage_Comisiones_Acumuladas = (Convert.ToInt32(ViewState["TotalPages"]) - 1);
+            MostrarListadoComisionesAcumuladas();
+            MoverValoresPaginacion_Comisiones_Acumuladas((int)OpcionesPaginacionValores_Comisiones_Acumuladas.UltimaPagina, ref LinkBlbPaginaActualVariavleDetalleutton2, ref lbCantidadPaginaVariableDetalle);
         }
 
         protected void txtCodigoIntermediarioComisiones_TextChanged(object sender, EventArgs e)
